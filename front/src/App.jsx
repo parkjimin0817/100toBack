2b18a09dc0f5c49a2631812310190f2766b40e9b
euicheol99 <dmlcjfghskfe@naver.com>
2025-06-12 09:24:21 +0900

    no message

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/common/LoginPage';
 
