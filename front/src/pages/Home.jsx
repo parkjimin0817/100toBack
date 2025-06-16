93b86a2f688f039777ee868885fae6f4ce4aaea8
aralion <aadsf1789@gmail.com>
2025-06-16 09:00:26 +0900

    feat : 게시글 테이블 완성 및 사용 예시 작성

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import ContentHeader from '../components/Common/ContentHeader';
+import BoardTable from '../components/Board/BoardTable';
+import theme from "../styles/theme";
+
+const columns = [
+  {
+    label: '번호',
+    key: 'id',
+    width: '100px',
+    align: 'center'
+  },
+  {
+    label: '제목',
+    key: 'title',
+    // width: '60px',
+    align: 'left',
+  },
+  {
+    label: '작성자',
+    key: 'writer',
+    width: '100px',
+  },
+];
+
+const BoardData = [
+  { id : 1, title : "test1", writer : "정형일"},
+  { id : 2, writer : "정형일", title : "test2"},
+  { id : 3, title : "test3", writer : "정형일"},
+];
+
+const tableInfo = {
+  color : theme.colors.white,
+  backgroundColor : theme.colors.green,
+  thFontSize : theme.fontSizes.lg,
+  tbFontSize : theme.fontSizes.base,
+}
 
 const Home = () => {
 
@@ -16,12 +51,26 @@ const Home = () => {
           ]
         }
       ></ContentHeader>
+      
+      <BoardContainer>
+        <BoardTable 
+          tableInfo={tableInfo}
+          columns={columns}
+          boardData={BoardData}
+        />
+      </BoardContainer>
     </Content>
   );
 };
 
 const Content = styled.div`
   width: 100%;
+`;
+
+const BoardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
 `;
 
 export default Home;
