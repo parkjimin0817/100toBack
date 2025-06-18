082d156e8fe9dcc036e4860c8788b55a9ef7e831
Ksg <Tropicana1235>
2025-06-18 19:51:47 +0900

    fetch - front(교사 개인 일정관리)

--- front/src/pages/common/CustomCalendar.jsx
+++ front/src/pages/common/CustomCalendar.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-const CustomCalendar = ({ scheduleData, onDateClick }) => {
+const CustomCalendar = ({ onDateClick }) => {
   return (
     <StyledCalendar
       calendarType="gregory"
