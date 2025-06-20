6891eb03cdfd74fd58661b8539468b6da4c58df1
DESKTOP-QK533TD\User <jimin0817@naver.com>
2025-06-20 15:38:06 +0900

    chore : 기타

---
+++ back/kinder/src/main/java/com/bridge/kinder/controller/CenterController.java
@@ -0,0 +1,27 @@
+package com.bridge.kinder.controller;
+
+import com.bridge.kinder.dto.CenterDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/center")
+@RequiredArgsConstructor
+public class CenterController {
+
+    //시설 목록 가져오기
+    @GetMapping("/centers")
+    public ResponseEntity<List<CenterDto.Response>> getCenters(){
+
+    }
+
+
+
+}
