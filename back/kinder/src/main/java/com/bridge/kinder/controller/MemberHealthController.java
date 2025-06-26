b0bb14780b20b6b42e0de202ff2627727fd73757
Ksg <Tropicana1235>
2025-06-26 15:33:25 +0900

    create - memberHealthLog

--- back/kinder/src/main/java/com/bridge/kinder/controller/MemberHealthController.java
+++ back/kinder/src/main/java/com/bridge/kinder/controller/MemberHealthController.java
@@ -1,5 +1,18 @@
 package com.bridge.kinder.controller;
 
+import com.bridge.kinder.service.MemberHealthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/members/health")
+@RequiredArgsConstructor
 public class MemberHealthController {
+
+    private MemberHealthService memberHealthService;
+
 
 }
---
+++ back/kinder/src/main/java/com/bridge/kinder/service/MemberHealthServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bridge.kinder.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberHealthServiceImpl implements MemberHealthService {
+
+
+}
