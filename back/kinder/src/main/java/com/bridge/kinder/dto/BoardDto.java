3928c053beac44b40d1ee6823d50daac4dc515cd
aralion <aadsf1789@gmail.com>
2025-06-29 19:20:33 +0900

    Feat : (Back) 공지사항, 가정통신문, 알림장 추가

--- back/kinder/src/main/java/com/bridge/kinder/dto/BoardDto.java
+++ back/kinder/src/main/java/com/bridge/kinder/dto/BoardDto.java
@@ -46,6 +46,33 @@ public class BoardDto {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
+    public static class Update {
+        private Integer boardNo;
+        private String title;
+        private CommonEnums.BoardType type;
+        private String attachment;
+        private int centerId;
+        private Integer classRoomId;
+        private int memberId;
+        private List<BoardContentDto.Update> contents;
+
+        public Board toEntity(Center center, Member member, ClassRoom classRoom) {
+            return Board.builder()
+                    .title(this.title)
+                    .type(this.type)
+                    .attachment(this.attachment)
+                    .center(center)
+                    .classRoom(classRoom)
+                    .member(member)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
     public static class Detail {
         private Integer boardNo;
         private String title;
@@ -55,6 +82,7 @@ public class BoardDto {
         private String attachment;
         private Integer centerId;
         private String className;
+        private Integer memberNo;
         private String memberName;
         private List<BoardContentDto.Detail> boardContents;
 
@@ -68,6 +96,7 @@ public class BoardDto {
                     .attachment(board.getAttachment())
                     .centerId(board.getCenter() != null ? board.getCenter().getCenterNo() : null)
                     .className(board.getClassRoom() != null ? board.getClassRoom().getClassName() : null)
+                    .memberNo(board.getMember() != null ? board.getMember().getMemberNo() : null)
                     .memberName(board.getMember() != null ? board.getMember().getMemberName() : null)
                     .boardContents(
                             board.getBoardContents().stream()
@@ -113,7 +142,7 @@ public class BoardDto {
         private CommonEnums.BoardType type;  // 게시판 타입 (NOTICE)
         private int views;              // 조회수
         private LocalDateTime createDate; // 작성일
-        private String writerName;      // 작성자 이름
+        private String memberName;      // 작성자 이름
         private String attachment;      // 첨부파일 경로 또는 이름
 
         public static NoticeBoardDto fromEntity(Board board) {
@@ -123,8 +152,64 @@ public class BoardDto {
                     .type(board.getType())
                     .views(board.getViews())
                     .createDate(board.getCreateDate())
-                    .writerName(board.getMember() != null ? board.getMember().getMemberName() : null)
+                    .memberName(board.getMember() != null ? board.getMember().getMemberName() : null)
                     .attachment(board.getAttachment())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class NoteBoardDto { // 알림장 DTO
+
+        private int boardNo;                  // 게시물 번호
+        private String memberName;           // 작성자 이름
+        private String title;                // 제목
+        private LocalDateTime createDate;    // 작성일
+        private int views;                   // 조회수
+        private CommonEnums.BoardType type;  // 게시물 타입
+
+        public static NoteBoardDto fromEntity(Board board) {
+            return NoteBoardDto.builder()
+                    .boardNo(board.getBoardNo())
+                    .memberName(board.getMember() != null ? board.getMember().getMemberName() : null)
+                    .title(board.getTitle())
+                    .createDate(board.getCreateDate())
+                    .views(board.getViews())
+                    .type(board.getType())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class FamilyNoticeDto { // 가정통신문 DTO
+
+        private int boardNo;                   // 게시물 번호
+        private String className;             // 반 이름
+        private String attachment;            // 첨부 파일
+        private String memberName;            // 작성자
+        private String title;                 // 제목
+        private LocalDateTime createDate;     // 작성일
+        private int views;                    // 조회수
+        private CommonEnums.BoardType type;   // 게시물 타입
+
+        public static FamilyNoticeDto fromEntity(Board board) {
+            return FamilyNoticeDto.builder()
+                    .boardNo(board.getBoardNo())
+                    .className(board.getClassRoom() != null ? board.getClassRoom().getClassName() : null)
+                    .attachment(board.getAttachment())
+                    .memberName(board.getMember() != null ? board.getMember().getMemberName() : null)
+                    .title(board.getTitle())
+                    .createDate(board.getCreateDate())
+                    .views(board.getViews())
+                    .type(board.getType())
                     .build();
         }
     }
