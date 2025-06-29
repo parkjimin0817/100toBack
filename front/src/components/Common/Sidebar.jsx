4a4e6cdc830f1eb11a2c455de96a2f71ecf13d0e
aralion <aadsf1789@gmail.com>
2025-06-29 19:24:08 +0900

    Feat : 사이드바 페이지 연결

--- front/src/components/Common/Sidebar.jsx
+++ front/src/components/Common/Sidebar.jsx
@@ -162,10 +162,10 @@ const managerSidebar = [
     icon: <MdFilterFrames />,
     color: 'green',
     subItems: [
-      { label: '공지사항', link: '*' },
+      { label: '공지사항', link: '/notice/list' },
       { label: '가정통신문', link: '/family_notice/list' },
       { label: '식단표', link: '*' },
-      { label: '알림장', link: '*' },
+      {label : "알림장", link : "/note/list"},
       { label: '사진 게시판', link: '*' },
     ],
   },
@@ -223,10 +223,10 @@ const parentSidebar = [
     icon: <MdFilterFrames />,
     color: 'purple',
     subItems: [
-      { label: '공지사항', link: '*' },
+      { label: '공지사항', link: '/notice/list' },
       { label: '가정통신문', link: '/family_notice/list' },
       { label: '식단표', link: '*' },
-      { label: '알림장', link: '*' },
+      {label : "알림장", link : "/note/list"},
     ],
   },
   {
