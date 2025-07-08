62df15e48b5019357bfb15bbfa25d5fb69d4dc34
aralion <aadsf1789@gmail.com>
2025-07-09 03:37:58 +0900

    Fix : 페이지네이션 컴포넌트도 컨텐츠 헤더의 색을 따라갈 수 있도록 수정

--- front/src/pages/NotePage.jsx
+++ front/src/pages/NotePage.jsx
@@ -93,7 +93,12 @@ const NotePage = () => {
         </BoardContainer>
       )}
       {data && (
-        <Pagination currentPage={data.number + 1} totalPages={data.totalPages} onPageChange={handlePageChange} />
+        <Pagination
+          currentPage={data.number + 1} 
+          totalPages={data.totalPages} 
+          onPageChange={handlePageChange} 
+          Color={member.memberType === 'PARENT' ? 'purple' : 'green'}
+        />
       )}
     </PageContainer>
   );
