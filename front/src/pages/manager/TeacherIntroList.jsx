08870f797cf4e912c1ffef06e0ef419b99959786
didwlwd <didehdals321@naver.com>
2025-07-15 09:15:06 +0900

    refactor : 회원가입 시 시설 전화번호 입력 오류 수정

--- front/src/pages/manager/TeacherIntroList.jsx
+++ front/src/pages/manager/TeacherIntroList.jsx
@@ -62,7 +62,7 @@ export default TeacherIntroList;
 
 const Content = styled.div`
   width: 100%;
-  min-height: 840px;
+  min-height: 600px;
   background-color: #ffffff;
   border-radius: 20px;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
