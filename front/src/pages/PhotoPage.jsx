dc7ab33e3210618d6100e1b4181e65090a99b307
parkjimin0817 <jimin0817@naver.com>
2025-07-16 16:44:12 +0900

    Update PhotoPage.jsx

--- front/src/pages/PhotoPage.jsx
+++ front/src/pages/PhotoPage.jsx
@@ -21,7 +21,7 @@ const PhotoPage = () => {
         setData(responseData);
         // alert("게시글 조회 성공");
       } catch (error) {
-        toast.error('게시글 조회 실패 : ', error);
+        console.error('게시글 조회 실패 : ', error);
         alert('게시글 조회 실패');
       }
     };
