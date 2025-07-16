65c927eb1f562f70c0e005e9af6384097dd31167
parkjimin0817 <jimin0817@naver.com>
2025-07-16 16:45:24 +0900

    Update FamilyNoticePage.jsx

--- front/src/pages/teacher/FamilyNoticePage.jsx
+++ front/src/pages/teacher/FamilyNoticePage.jsx
@@ -74,7 +74,7 @@ const FamilyNoticePage = () => {
         setData(responseData);
         // alert("게시글 조회 성공");
       } catch (error) {
-        toast.error('게시글 조회 실패 : ', error);
+        console.error('게시글 조회 실패 : ', error);
         alert('게시글 조회 실패');
       }
     };
