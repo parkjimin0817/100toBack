baf04c8b71ec23d95f86a689b24e83d6de3542b9
aralion <aadsf1789@gmail.com>
2025-07-16 16:47:03 +0900

    Fix : 채팅방 UI 수정

--- front/src/components/Chat/ChatFooter.jsx
+++ front/src/components/Chat/ChatFooter.jsx
@@ -70,9 +70,11 @@ const MessageInput = styled.input`
   border: 1px solid #ccc;
   border-radius: 5px;
   margin-right: 10px;
+  width: 150px;
 `;
 
 const SendButton = styled.button`
+  width : 50px;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
