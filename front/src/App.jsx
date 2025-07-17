56e2c2e7e2ed1300c27e54a1d93070c6f067b225
aralion <aadsf1789@gmail.com>
2025-07-17 11:46:58 +0900

    Feat : 학부모, 교사, 시설장 마이페이지 하나로 통합

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -67,6 +67,7 @@ import CouncelClassList from './pages/manager/CouncelClassList';
 import CouncelSettingPage from './pages/manager/CouncelSettingPage';
 import ParentCouncelPage from './pages/parent/ParentCouncelPage';
 import TeacherDocument from './pages/teacher/TeacherDocument';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
@@ -88,7 +89,7 @@ function App() {
              * */}
             <Route path="/parent/main" element={<ParentMainPage />} />
             <Route path="/parent/mychild" element={<ParentChildList />} />
-            <Route path="/parent/mypage" element={<ParentMyPage />} />
+            <Route path="/parent/mypage" element={<MyPage />} />
             <Route path="/councel/parent" element={<ParentCouncelPage />} />
 
             {/**
@@ -103,6 +104,7 @@ function App() {
              * 7. 교사 개인, 유치원 일정 관리 페이지
              * 8. 교사 근태 관리 페이지
              * 9. 교사 마이페이지
+             * 10. 교사 서류 관리
              *
              * */}
             <Route path="/teacher/main" element={<TeacherMainPage />} />
@@ -113,7 +115,7 @@ function App() {
             <Route path="/teacher/parentcontactinfo" element={<ParentContact />} />
             <Route path="/scheduleTeacher" element={<ScheduleTeacher />} />
             <Route path="/teacher/myattendance" element={<MyAttendance />} />
-            <Route path="/teacher/mypage" element={<TeacherMyPage />} />
+            <Route path="/teacher/mypage" element={/*<TeacherMyPage />*/ <MyPage />}  />
             <Route path="/teacher/mydocument" element={<TeacherDocument />} />
             {/**
              * 시설장
@@ -133,7 +135,7 @@ function App() {
              *
              * */}
             <Route path="/manager/main" element={<TeacherMainPage />} />
-            <Route path="/manager/mypage" element={<TeacherMyPage />} />
+            <Route path="/manager/mypage" element={/*<TeacherMyPage />*/ <MyPage />} />
             <Route path="/manager/classplacement" element={<ClassPlacement />} />
             <Route path="/manager/teacherlist" element={<TeacherList />} />
             <Route path="/approvalList" element={<ApprovalList />} />
